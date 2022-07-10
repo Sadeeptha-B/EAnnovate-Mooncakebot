b019bdeaba3add4c944eb3a88b4de2c377460b79
Kaveesha <gnis0001@student.monash.edu>
2022-07-10 15:13:52 +0800

    Mooncake Bot order function

--- MoonCakeBot/mooncake_bot.js
+++ MoonCakeBot/mooncake_bot.js
@@ -17,8 +17,43 @@ client.on("messageCreate", msg => {
     if(msg.content === "!menu"){
         msg.channel.send({embeds: [createEmbed("We have 3 Different flavors available: \n\n• White Lotus \n• Red Bean \n• Green Tea", "Menu")]})
     }
+    if(msg.content === "!order"){
+      msg.channel.send(getOrderButton());
+    }
 
+    if(msg.content === "!summary"){
+        msg.channel.send({embeds: []})
+    }
 })
+
+client.on('interactionCreate', async interaction =>{
+  console.log(interaction.customId)
+  if(interaction.isButton && interaction.customId === "Order"){
+    await interaction.showModal(createModal(),{
+      client: client,
+      interaction: interaction
+    })
+  }
+})
+
+client.on('interactionCreate', interaction => {
+	if (!interaction.isModalSubmit()) return;
+  if(interaction.customId === "OrderModal"){
+    const quantityWL = interaction.fields.getTextInputValue("WhiteLotus")
+    const quantityRB = interaction.fields.getTextInputValue("RedBean")
+    const quantityGT = interaction.fields.getTextInputValue("GreenTea")
+    console.log(quantityWL, quantityRB, quantityGT)
+    interaction.reply({content: "Your Order was successful."})
+  }
+});
+
+
+function getOrderButton(){
+  return {content: 'Click the button below to order some mooncake!',
+  components:[{ type: 1, components: [{
+    type: 2, custom_id: "Order", label: 'Order', style: 'PRIMARY',
+  }]}]}
+}
 
 function createEmbed(message, title){
     return new Discord.MessageEmbed()
@@ -26,6 +61,56 @@ function createEmbed(message, title){
     .setTitle(title)
     .setDescription(message)
     .setTimestamp()
+}
+
+
+
+function createModal(){
+    const modal = new Discord.Modal()
+    .setCustomId("OrderModal")
+    .setTitle("Order Mooncakes from us")
+    components = [
+        {
+          type: 1,
+          components: [{
+            type: 4, 
+            custom_id: 'WhiteLotus',
+            label: `White Lotus Mooncakes`,
+            style: 1,
+            min_length: 1,
+            max_length: 3,
+            placeholder: '0',
+            required: true
+          }],
+        },
+        {
+          type: 1,
+          components: [{
+            type: 4,
+            custom_id: 'RedBean',
+            label: `Red Bean Mooncakes`,
+            style: 1,
+            min_length: 1,
+            max_length: 3,
+            placeholder: '0',
+            required: true
+          }]
+        },
+        {
+          type: 1,
+          components: [{
+            type: 4,
+            custom_id: 'GreenTea',
+            label: `Green Tea Mooncakes`,
+            style: 1,
+            min_length: 1,
+            max_length: 3,
+            placeholder: '0',
+            required: true
+          }]
+        }]
+    modal.addComponents(components)
+    return modal
 }
 
 client.login(process.env.TOKEN)
