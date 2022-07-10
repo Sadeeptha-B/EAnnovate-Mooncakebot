28719c87679249dc6cb49565ab185b7f48f8f5c9
Sadeeptha <hban0006@student.monash.edu>
2022-07-11 00:25:11 +0530

    Moved model event listener to one used by the button, added sql dependencies

--- mooncake_bot.js
+++ mooncake_bot.js
@@ -10,49 +10,45 @@ client.once('ready', () => {
 const commandPrefix = "!"
 
 client.on("messageCreate", msg => {
-    if(!msg.content.startsWith(commandPrefix)){
-        return
-    }
+    if(!msg.content.startsWith(commandPrefix)) return
+    const command = msg.content.substring(1);
 
-    if(msg.content === "!help"){
+    if(command === "help"){
         msg.channel.send({embeds: [createEmbed(("!help - Provides users with a list of valid commands.\n!menu - Displays a menu for users to view the available mooncakes.\n!order - This command allows users to place their orders.\n!summary - Provides a list of all previous orders a user has made."), "help")]})
     }
-    if(msg.content === "!menu"){
+    if(command === "menu"){
         msg.channel.send({embeds: [createEmbed("We have 3 Different flavors available: \n\n• White Lotus \n• Red Bean \n• Green Tea", "Menu")]})
     }
-    if(msg.content === "!order"){
+    if(command === "order"){
       msg.channel.send(getOrderButton());
     }
 
-    if(msg.content === "!summary"){
+    if(command === "summary"){
         msg.channel.send({embeds: []})
     }
 })
 
 client.on('interactionCreate', async interaction =>{
-  console.log(interaction.customId)
   if(interaction.isButton && interaction.customId === "Order"){
     await interaction.showModal(createModal(),{
       client: client,
       interaction: interaction
     })
   }
-})
 
-client.on('interactionCreate', interaction => {
-	if (!interaction.isModalSubmit()) return;
-  if(interaction.customId === "OrderModal"){
+  if(interaction.isModalSubmit() && interaction.customId === "OrderModal"){
     const quantityWL = interaction.fields.getTextInputValue("WhiteLotus")
     const quantityRB = interaction.fields.getTextInputValue("RedBean")
     const quantityGT = interaction.fields.getTextInputValue("GreenTea")
     console.log(quantityWL, quantityRB, quantityGT)
-    interaction.reply({content: "Your Order was successful."})
+    await interaction.reply({content: "Your Order was successful."})
   }
-});
+})
 
 
 function getOrderButton(){
   return {content: 'Click the button below to order some mooncake!',
+  //ephemeral: true,
   components:[{ type: 1, components: [{
     type: 2, custom_id: "Order", label: 'Order', style: 'PRIMARY',
   }]}]}
